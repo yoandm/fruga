4d1ab0abfdee5122a922358223385e0a15ea2b37
Yoan De Macedo <mail@yoandm.com>
2025-04-10 07:31:34 +0200

    move autoload

--- fruga.php
+++ fruga.php
@@ -18,7 +18,29 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
-    require 'autoload.php';
+
+    define('DIR_APP', dirname(__FILE__));
+    
+    if(!defined('DS')) {
+            define('DS', DIRECTORY_SEPARATOR);
+    }
+
+    spl_autoload_register(function ($class) {
+        if(substr($class, 0, 6) === 'Yoandm'){
+            $parts = explode('\\', $class);
+            $path = '';
+            for($i = 2; $i < count($parts) - 1; $i ++)
+                        $path .= $parts[$i] . DS;
+
+            $path .= $parts[$i] . '.php';
+
+            if(file_exists(DIR_APP . DS . 'src' . DS .  $path))
+                require DIR_APP . DS . 'src' . DS . $path;
+        } else if($class === 'Parsedown'){
+            require DIR_APP . DS . 'lib' . DS . 'parsedown/Parsedown.php';
+        }
+    });
+
 
     use Yoandm\Fruga\Generation\Generator;
     use Yoandm\Fruga\Deployment\Deployer;
